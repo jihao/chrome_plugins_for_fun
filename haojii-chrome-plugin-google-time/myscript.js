48cc867c5313d240c62edfe3a0dd3215015f5201
jihao <jacky.jihao@gmail.com>
2012-01-12 22:28:05 +0800

    my first and second chrome plugin(display current time above google search box)

---
+++ haojii-chrome-plugin-google-time/myscript.js
@@ -0,0 +1,27 @@
+function startTime()
+ {
+    var today = new Date()
+    var h = today.getHours()
+    var m = today.getMinutes()
+    var s = today.getSeconds()
+    // add a zero in front of numbers<10
+    m = checkTime(m)
+    s = checkTime(s)
+    // update google
+    $("#current_time").html( h + ":" + m + ":" + s);
+    t = setTimeout('startTime()', 500)
+}
+
+function checkTime(i)
+ {
+    if (i < 10)
+    {
+        i = "0" + i
+    }
+    return i
+}
+
+$(document).ready(function() {
+	$("#lga").after("<p id='current_time'></p>")
+    startTime();
+});
