504946508cd73a6ca3f89451833cfedd312a478e
jihao <jacky.jihao@gmail.com>
2012-02-10 12:59:48 +0800

    updated amazon payment faker, change window location instead of dynamicly modify web page

--- amazon-payment-faker/myscript.js
+++ amazon-payment-faker/myscript.js
@@ -1,11 +1,14 @@
-var phoneURL = chrome.extension.getURL("images/phone.png");
-var applyURL = chrome.extension.getURL("images/apply.png");
-
 $(document).ready(function() {
-	//console.log($("#other-payment-options"));
-	
-	var $html = '<div class="payment-method" id="ericsson_mobile" style=""><img class="logo"  alt="" src="'+phoneURL+'"><h2 id="title-gift-card">Pay by Mobile</h2><div style="margin-left: 118px;"><p class="caption">Phone Number: xxxxxxxx</p><p class="caption">Payment Password</p></div><div class="offer" id="gcpromo"><input type="text" size="25" maxlength="25" id="gcpromoinput" title="phone number"><input type="image" class="apply action_link" style="vertical-align: text-bottom;"  alt="apply" src="'+applyURL+'"><p class="caption" id="title-new-gift-card">Pay by mobile through Ericsson Mobile Orchestrator.</p></div><br></div>';
-	
-	$("#other-payment-options").after ( $html );
-   //$("#other-payment-options").after(  );
+  console.log($(".rightbutton"));
+   
+   $(".rightbutton, #continue").live('click',function(event){
+      event.preventDefault();
+      event.stopPropagation();
+      window.location.href="http://localhost:8080/amazon-mobile-payment";
+   });
+   ("p .continue").live('click',function(event){
+      event.preventDefault();
+      event.stopPropagation();
+      window.location.href="http://localhost:8080/amazon-mobile-payment";
+   });
 });
